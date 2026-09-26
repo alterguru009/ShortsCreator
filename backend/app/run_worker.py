be8c9c43399b4a29bdfd9ee1a6bb085f2840aa8d
alterguru009 <u32519695@gmail.com>
2026-09-26 11:28:29 +0530

    Update run_worker.py

--- backend/app/run_worker.py
+++ backend/app/run_worker.py
@@ -9,5 +9,5 @@ worker.start()
 
 print("Worker started. Processing jobs for 5 minutes...")
 # 5 minute tak chalao taaki job complete ho sake
-time.sleep(300) 
+time.sleep(600) 
 print("Worker finished.")
